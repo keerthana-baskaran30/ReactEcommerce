f03b1a5f3af133ee01c231eb4a8dc4270acd7164
Keerthana-30 <71180623+Keerthana-30@users.noreply.github.com>
2022-11-02 12:20:50 +0530

    comitih

--- ecommerce/src/store/store.js
+++ ecommerce/src/store/store.js
@@ -9,6 +9,6 @@ const middlewares = [thunk];
 if (process.env.NODE_ENV === "development") middlewares.push(logger);
 
 export const store = createStore(
-  rootReducer,bjj,
+  rootReducer,
   composeWithDevTools(applyMiddleware(...middlewares))
 );
